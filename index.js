fdfb74b0914ab7878deb2fc059bea85402220b82
Aaron Parecki <aaron@parecki.com>
2013-03-18 13:37:31 -0700

    Moves people to config file, adds atom route for showing status.

--- index.js
+++ index.js
@@ -12,13 +12,23 @@ var bricks = require('bricks'),
     fs     = require('fs'),
     url    = require('url');
 
+var XMLWriter = require('xml-writer'),
+    ATOMWriter = require('atom-writer');
+
+var dataStore = "./data/store.json";
+var lastModified = new Date();
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
 
 function writeData (store) {
-  fs.writeFileSync("./data/store.json", JSON.stringify(store), 'utf8');
+  fs.writeFileSync(dataStore, JSON.stringify(store), 'utf8');
+  lastModified = new Date();
 }
 
 function readData () {
-  var data = fs.readFileSync("./data/store.json", 'utf8');
+  var data = fs.readFileSync(dataStore, 'utf8');
   var store;
   try {
     store = JSON.parse(data);
@@ -78,6 +88,49 @@ function modifyRoute (request, response) {
   response.end();
 }
 
+function atomRoute (request, response) {
+  var xwriter = new XMLWriter(true);
+  var atom = new ATOMWriter(xwriter);
+
+  var data = [];
+  for(var y in store) {
+    for(var x in store[y]) {
+      if(store[y][x] > 0) {
+        data.push(y.toString().capitalize()+" owes "+x.capitalize()+" "+store[y][x]+" coffee"+(store[y][x] > 1 ? "s" : ""));
+      }
+    }
+  }
+
+  atom
+    .startFeed('urn:coffee', lastModified, lastModified)
+    .writeStartIndex(1)
+    .writeItemsPerPage(data.length)
+    .writeTotalResults(data.length)
+    .writeTitle("Coffee Board")
+    .writeLink(config.baseURL+"score.atom", "application/atom+xml", "self")
+
+  for(var i in data) {
+    atom
+      .startEntry("urn:coffee:"+lastModified.toISOString()+":"+i, lastModified, lastModified)
+      .writeTitle(data[i])
+      .writeContent(data[i])
+      .writeAuthor("Esri Portland")
+      .endEntry()
+  }
+
+  atom.endFeed();
+
+  response.setHeader('Content-Type', 'text/xml');
+  response.write(atom.writer.toString());
+  response.end();
+}
+
+function peopleRoute (request, response) {
+  response.setHeader('Content-Type', 'application/json');
+  response.write(JSON.stringify(config.people));
+  response.end();
+}
+
 var redirects = [
   {
     path: "^/$",
@@ -88,10 +141,14 @@ var redirects = [
 appServer.addRoute(".+", appServer.plugins.redirect, { section: "pre", routes: redirects });
 
 appServer.addRoute(".+", appServer.plugins.filehandler, { basedir: "./htdocs" });
-appServer.addRoute("/score", scoreRoute);
+appServer.addRoute("/score$", scoreRoute);
+appServer.addRoute("/score\.atom", atomRoute);
 appServer.addRoute("/coffee", modifyRoute);
+appServer.addRoute("/people", peopleRoute);
 appServer.addRoute(".+", appServer.plugins.fourohfour);
 
+appServer.addEventHandler('route.fatal', function (error) { console.log("FATAL: " + error); console.dir(error); });
+appServer.addEventHandler('run.fatal', function (error) { console.log("FATAL: " + error); console.dir(error); });
 
 var server = appServer.createServer();
 server.listen(config.listen);

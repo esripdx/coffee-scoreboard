c0c1b3a11fa6e1df31c52eb548eb8643dca623c6
Nate Goldman <nnmgoldman@gmail.com>
2013-03-20 17:28:19 -0700

    bonsai meets jquery & css

--- htdocs/js/coffee.js
+++ htdocs/js/coffee.js
@@ -1,62 +1,106 @@
-function createPeople(where, people) {
-  var $container = $(where);
+function renderBoard(people, scores) {
+  var stage = bonsai.run(document.getElementById('shadow'), {
+    code: function() {
+      x = stage.width/2;
+      y = stage.height/2;
+      r = stage.height/2.5;
 
-  var html = '<div class="portrait"><h2 class="name"></h2><div class="coffee-token" draggable="true"></div></div>';
+      function deg2rad(deg){
+        return deg * (Math.PI/180);
+      }
 
-  var $template = $('<div/>').attr({
-    'class': 'person',
-  }).html(html);
+      function rad2deg(rad){
+        return rad * (180/Math.PI);
+      }
 
-  for (i = 0; i < people.length; i++) {
-    var $el = $template.clone();
+      stage.sendMessage("ready", {});
+      stage.on("message:data", function(data){
 
-    $el
-      .attr({ 'data-name': people[i] })
-      .find('.name')
-        .text(people[i])
-        .end()
-      .find('.coffee-token')
-        .on('dragstart',function(e){
-          console.log('dragging');
-        })
-        .on('dragend', function(e){
-          console.log('dragend');
-        })
-        .end()
-      .on('drop', function(e){
-        if (e.preventDefault) e.preventDefault();
-        console.log('drop');
-      });
+        var start = 0;
+        var end = deg2rad(360/data.length);
 
-    $container.append($el);
-  }
+        for (var i = 0; i < data.length; i++) {
+          var arcPoint1 = new Arc(x, y, r, start, end).points()[0];
 
-  var $people = $('.person');
+          stage.sendMessage("circle", {
+            x: arcPoint1[0]+x,
+            y: arcPoint1[1]+y,
+            name: data[i].name,
+            email: data[i].email
+          });
 
-  numItems = $people.length;
-  start    = 0.25;
-  step     = ( 2 * Math.PI ) / numItems;
-
-  $people.each(function(index) {
-    radius  = ($container.width() - $(this).width())/2;
-    tmpTop  = (($container.height()/2) + radius * Math.sin(start)) - ($(this).height()/2);
-    tmpLeft = (($container.width()/2) + radius * Math.cos(start)) - ($(this).width()/2);
-
-    start += step;
-
-    $(this).css("top", tmpTop);
-    $(this).css("left", tmpLeft);
+          start = end;
+          end = start+deg2rad(360/data.length);
+        }
+      })
+    },
+    width: window.innerWidth,
+    height: window.innerHeight
   });
 
+  stage.on('load', function() {
+    stage.on("message:ready", function() {
+
+      var html = '<div class="portrait"><h2 class="name"></h2><div class="coffee-token" draggable="true"></div></div>';
+
+      var $template = $('<div/>').attr({
+        'class': 'person',
+      }).html(html);
+
+      stage.sendMessage("data", people);
+
+      stage.on('message:circle', function(data) {
+        var $el = $template.clone();
+
+        $el
+          .attr({ 'data-name': data.name })
+          .on('dragstart', function(e){
+            // var dragIcon = document.createElement('img');
+            // dragIcon.src = '/img/token.png';
+            // dragIcon.width = 100;
+            // e.originalEvent.dataTransfer.setDragImage(dragIcon, -10, -10);
+          })
+          .on('dragover', function(e){
+            e.preventDefault();
+            $(this).addClass('over');
+          })
+          .on('dragleave', function(e){
+            $(this).removeClass('over');
+          })
+          .on('drop', function(e){
+            if (e.preventDefault) e.preventDefault();
+            $(this).removeClass('over');
+          })
+          .find('.name')
+            .text(data.name)
+            .end()
+          // .find('.coffee-token')
+          //   .on('dragstart',function(e){
+          //     console.log('dragging');
+          //   })
+          //   .on('dragend', function(e){
+          //     console.log('dragend');
+          //   })
+          //   .end()
+          .find('.portrait')
+            .css({
+              "background": "url('"+gravatar(data.email, 50)+"')",
+              "top": data.y,
+              "left": data.x
+            })
+            .end()
+          .appendTo('#scoreboard');
+      });
+    });
+  });
+
+  console.log(scores);
 }
 
 $(document).ready(function() {
   $.get("/people", function(people) {
-    createPeople("#scoreboard", people);
-
     $.get("/score", function(scores) {
-      console.log(scores);
+      renderBoard(people, scores);
     });
-
-  })
+  });
 });

cc6a6aa0ff0109e080d7865a2fcd2333e7d32e42
Paul C Pederson <ppederson@esri.com>
2013-05-07 17:52:12 -0700

    hacky working drag icon

--- htdocs/js/touch.js
+++ htdocs/js/touch.js
@@ -1,22 +1,33 @@
 $(function(){
 
-  $(".drag")
-    .hammer({
-      drag_max_touches:0
-    })
-    .on("touch drag", function(ev) {
-        var touches = ev.gesture.touches;
 
-        //ev.gesture.preventDefault();
+  var i = setInterval(function ()
+  {
+      if ($('#list').length)
+      {
+          clearInterval(i);
 
-        console.log(this);
+          console.log("safe-now");
 
-        for(var t=0,len=touches.length; t<len; t++) {
-            var target = $(touches[t].target);
-            target.css({
-                left: touches[t].pageX-50
-            });
-        }
-    });
+          $(".drag")
+          .hammer({
+            drag_max_touches:0
+          })
+          .on("touch drag", function(ev) {
+              var touches = ev.gesture.touches;
+
+              ev.gesture.preventDefault();
+
+              console.log(this);
+
+              for(var t=0,len=touches.length; t<len; t++) {
+                  var target = $(touches[t].target);
+                  target.css({
+                      left: touches[t].pageX-50
+                  });
+              }
+          });
+      }
+  }, 100);
 
 });

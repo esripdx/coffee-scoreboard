4cd189dceb822d0bee6407a373b996d95dd51881
Nate Goldman <nnmgoldman@gmail.com>
2013-05-09 10:09:38 -0700

    near-working prototype

--- htdocs/js/mobile.js
+++ htdocs/js/mobile.js
@@ -1,5 +1,12 @@
 (function(window,$,undefined){
 
+  // makes click trigger tap on non-touch device
+  if (!('ontouchend' in window)) {
+    $(document).on('click', 'body', function(e) {
+      $(e.target).trigger('touchend');
+    });
+  }
+
   // the players
 
   var people, scores;
@@ -26,7 +33,7 @@
     var $scoreboard = $nav.find('.scoreboard');
     var $wants = $nav.find('.wants');
 
-    $scoreboard.on('click', function(e){
+    $scoreboard.on('touchend', function(e){
       e.preventDefault();
 
       $nav.find('.active').removeClass('active');
@@ -35,7 +42,7 @@
       $(this).addClass('active');
     });
 
-    $wants.on('click', function(e){
+    $wants.on('touchend', function(e){
       e.preventDefault();
 
       $nav.find('.active').removeClass('active');
@@ -44,7 +51,7 @@
       $(this).addClass('active');
     });
 
-    $logout.on('click', function(e){
+    $logout.on('touchend', function(e){
       e.preventDefault();
       logout();
     });
@@ -144,11 +151,13 @@
     $('#list').find('.card').each(function(){
       var $card = $(this);
       var $more = $card.find('.more');
-      $more.on('click', function(e){
+      $more.on('touchend', function(e){
         e.preventDefault();
         $card.toggleClass('active');
       })
     });
+
+    bindSwipe();
   }
 
   People.prototype.buildWants = function() {
@@ -191,7 +200,7 @@
       $auth.append(html);
     }
 
-    $auth.find('.person').on('click', function(e){
+    $auth.find('.person').on('touchend', function(e){
       e.preventDefault();
 
       var $el = $(this);
@@ -292,6 +301,142 @@
       }
     }
   }
+
+  // paul's touch
+
+  function bindSwipe() {
+    var hammertime = Hammer($('.slider'), {
+      transform_always_block: true,
+      transform_min_scale: 1,
+      drag_block_horizontal: true,
+      drag_block_vertical: true,
+      drag_min_distance: 0
+    });
+
+    hammertime.on('dragstart drag dragend', function(ev) {
+      var slider = this;
+      manageMultitouch(ev, slider);
+    });
+  }
+
+  function manageMultitouch(ev, slider){
+
+    var token      = $('.coffee-token', slider),
+        me         = $('.me', slider),
+        them       = $('.them', slider),
+        verb       = $('.verb', slider),
+        my_name    = me[0].getAttribute("data-user"),
+        their_name = them[0].getAttribute("data-user"),
+        card       = token.parent().parent().parent(),
+        badge      = $('.badges', card),
+        message    = $('.message', card);
+
+    var active_me = false;
+    var active_them = false;
+
+    switch(ev.type) {
+        case 'dragstart':
+          token.addClass('active');
+            break;
+
+        case 'drag':
+
+          var me_pos    = me.offset(),
+              them_pos  = them.offset(),
+              token_pos = token.offset(),
+              x         = ev.gesture.center.pageX;
+
+          // determine if token is inside the slider
+          if (x > me_pos.left + 18 && x < them_pos.left + them_pos.width - 20){
+            token.css('left', x - 50);
+          }
+
+          // determine if token is over me
+          if (token_pos.left < me_pos.left + me_pos.width && active_me === false) {
+            me.addClass("active");
+            window.active_me = true;
+          } else {
+            me.removeClass("active");
+            window.active_me = false;
+          }
+
+          // determine if token is over them
+          if (token_pos.left + token_pos.width > them_pos.left && active_them === false) {
+            them.addClass("active");
+            window.active_them = true;
+          } else {
+            them.removeClass("active");
+            window.active_them = false;
+          }
+
+          //change swipe to release when hovering
+          if (window.active_me === true || window.active_them === true) {
+            verb[0].innerHTML = 'release';
+          } else {
+            verb[0].innerHTML = 'swipe';
+          }
+
+            break;
+
+        case 'dragend':
+
+          token.removeClass('active');
+
+          //if hovering, execute appropriate coffee transfer
+          if (window.active_me === true){
+            updateScore(my_name, their_name, their_name, my_name, badge, message);
+            me.removeClass("active");
+          } else if (window.active_them === true){
+            updateScore(my_name, their_name, my_name, their_name, badge, message);
+            them.removeClass("active");
+          }
+            break;
+    }
+
+  }
+
+  function updateScore(me, them, from, to, b, m){
+    $.ajax({
+      type: 'GET',
+      url: "/coffee?from=" + from.toLowerCase() + "&to=" + to.toLowerCase(),
+      // data to be added to query string:
+      dataType: 'json',
+      timeout: 300,
+      success: function(data){
+
+        var their_name = them;
+        me = me.toLowerCase().toString();
+        them = them.toLowerCase().toString();
+
+        var my_credit = data[me][them];
+        var my_debt = data[them][me];
+        var coffee_word = " coffees";
+
+        if (my_credit > my_debt) {
+          if (my_credit == 1) { coffee_word = " coffee";}
+          b.removeClass("debts credits even updated").addClass("credits updated");
+          b[0].innerHTML = my_credit;
+          m[0].innerHTML = '<span class="name">' + their_name + ' </span>' + ' owes you ' + my_credit + coffee_word + ".";
+        } else if (my_debt > my_credit) {
+          if (my_debt == 1) { coffee_word = " coffee";}
+          b.removeClass("debts credits even updated").addClass("debts updated");
+          b[0].innerHTML = my_debt;
+          m[0].innerHTML = 'You owe <span class="name">' + their_name + '</span> ' + my_debt + ' ' + coffee_word + '.';
+        } else {
+          b.removeClass("debts credits even updated").addClass("even updated");
+          b[0].innerHTML = '=';
+          m[0].innerHTML = 'You and <span class="name">' + their_name + '</span> are even.';
+        }
+
+        // update the credit or debit score
+        // update the text in the card
+      },
+      error: function(xhr, type){
+        alert('Ajax error!');
+      }
+    });
+  }
+
 
   // the play
 
